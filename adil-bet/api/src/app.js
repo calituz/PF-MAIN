490f5933ef9ab54cf7ca1cdbdb24393a94fd475c
calituz <carlos.ciliberti@gmail.com>
2023-01-19 23:30:36 -0300

    update

--- adil-bet/api/src/app.js
+++ adil-bet/api/src/app.js
@@ -12,12 +12,7 @@ const routes = require('./routes');
 const server = express();
 
 // Cors options
-const corsOptions = {
-    origin: 'https://pf-main-production.up.railway.app/login/auth0',
-    credentials: true,
-    methods: 'GET, PATCH, POST, OPTIONS, PUT, DELETE',
-    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept'
-};
+
 
 // Middlewares
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
@@ -26,7 +21,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://pf-main-production.up.railway.app/login/auth0' /* 'http://localhost:3000' */); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Origin', '*' /* 'http://localhost:3000' */); // update to match the domain you will make the request from
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, PATCH, POST, OPTIONS, PUT, DELETE');
